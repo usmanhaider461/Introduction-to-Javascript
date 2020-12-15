85a00ec7565e00977070188356b7473243656bed
usmanhaider461 <46385291+usmanhaider461@users.noreply.github.com>
2020-12-15 13:46:13 +0500

    Functions and Higher Order Functions

--- function.js
+++ function.js
@@ -1,5 +1,38 @@
-function doSomething() {
-    console.log(" Hello World");
-}
+// function doSomething() {
+//     console.log(" Hello World");
+// }
 
-doSomething();
+// // doSomething();
+// doSomething();
+
+// function square(num) {
+//     console.log(num * num);
+//   }
+// function area(width, length) {
+//     console.log(width * length);
+//   }
+
+//   square(10);
+//   square(3);
+//   square(91);
+
+//   area(12,14);
+
+//   function isEven(num) {
+//       if(num % 2 === 0) {
+//         console.log("${num} Number is even");
+//       } else
+//         console.log(num + " Number is not even");
+//   }
+
+//   isEven(8);
+
+function kebabToSnake(string) {
+    return string.replace(/-/g,'_');
+  }
+
+  var string = "hello-world-";
+    
+
+ string = kebabToSnake(string);
+ console.log(string);
---
+++ higher-order-func.js
@@ -0,0 +1,8 @@
+function singSong() {
+    console.log("twinkle twinkle ...");
+    console.log("How I wonder ...");
+}
+
+setInterval(function () {  
+    console.log("Here is an anonymous function");
+},1000) // 1000 in 1000ms=1s
